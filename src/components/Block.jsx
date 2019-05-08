8d5c87ed6f09992e35ed31a268a2947fed1e3373
Tracy Reith <treith@treith.com>
2019-05-07 21:06:22 -0700

    Clean up code

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -19,7 +19,7 @@ class Block extends React.Component {
     }
 
     return (
-      // I don't understand how 'background' (the falsy value) is getting passed to className if state = false for 'fill' is not being passed down from App
+
       <div className={(this.state.fill) ? 'foreground' : 'background'} onClick = {this.handleClick}>
         <style jsx='true'> {`
             .foreground  {
