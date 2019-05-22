6e8a52486a4f8079c4b53a5219c39c5700bcf644
Student <example@example.com>
2019-05-22 14:02:45 -0700

    Get at properties of inner array elements

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,39 +3,44 @@ import Block from './Block';
 import PropTypes from 'prop-types';
 
 function Grid(props)  {
-  console.log(props);
 
-
-    // const rows = Object.keys({props.gridList.gridId}).length;
 
     return (
       <div className='stitchGrid' >
 
-        {Object.keys(props.gridList).map(function(gridId){
-          const grid = props.gridList[gridId];
-          return <Block
-            fill = {grid.fill}
-            key = {grid.key}
-          />;
+        {Object.keys(props.gridList).map(function(gridId,j){
+          console.log(props.gridList[gridId][j]);
+          console.log('length of st array ' + props.gridList[gridId][j].length);
+          console.log('length of r array ' + props.gridList[gridId].length);
+          const st = props.gridList[gridId][j].length;
+          const r = props.gridList[gridId].length;
+          {Object.keys([gridId][j]).map(function(i,key){
+            console.log(props.gridList[gridId][j][i]);
+            let block = props.gridList[gridId][j][i];
+            return <Block
+              fill = {block.fill}
+              key = {key}
+            />
+          })}
         })}
         <style jsx='true'> {`
-            .stitchGrid {
-              display: grid;
-              grid-template-columns: repeat(8,1fr);
-              grid-template-rows: repeat(10, 1fr);
-              grid-gap: 1px 1px;
-              align-items: stretch;
-              height: 80vw;
-              width: 80vw;
-              min-width: 300px;
-              min-height: 300px;
-              max-width: 500px;
-              max-height: 500px;
-              background: #999;
-              border: 1px solid #999;
-            }
-            `}
-          </style>
+          .stitchGrid {
+            display: grid;
+            grid-template-columns: repeat( 8,1fr);
+            grid-template-rows: repeat(10, 1fr);
+            grid-gap: 1px 1px;
+            align-items: stretch;
+            height: 80vw;
+            width: 80vw;
+            min-width: 300px;
+            min-height: 300px;
+            max-width: 500px;
+            max-height: 500px;
+            background: #999;
+            border: 1px solid #999;
+          }
+        `}
+        </style>
       </div>
     );
   }
